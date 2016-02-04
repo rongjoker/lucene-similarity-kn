5ced2ed505736738144e91577e367027086db30c
rongjoker <magic_of_java@163.com>
2016-02-04 16:39:43 +0800

    in 2016/02/04

--- src/main/java/com/king/net/solr/similarity/DownloadsSolrSimilarity.java
+++ src/main/java/com/king/net/solr/similarity/DownloadsSolrSimilarity.java
@@ -3,6 +3,8 @@ package com.king.net.solr.similarity;
 import org.apache.lucene.index.FieldInvertState;
 import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.apache.lucene.util.BytesRef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author rongjoker
@@ -13,13 +15,17 @@ import org.apache.lucene.util.BytesRef;
  */
 public class DownloadsSolrSimilarity extends DefaultSimilarity {
 
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+
 	@Override
 	public float coord(int overlap, int maxOverlap) {
+		LOG.info("overlap:{};maxOverlap:{}", overlap, maxOverlap);
 		return super.coord(overlap, maxOverlap);
 	}
 
 	@Override
 	public float queryNorm(float sumOfSquaredWeights) {
+		LOG.info("sumOfSquaredWeights:{}", sumOfSquaredWeights);
 		return super.queryNorm(sumOfSquaredWeights);
 	}
 
@@ -32,31 +38,42 @@ public class DownloadsSolrSimilarity extends DefaultSimilarity {
 	 * 关键词出现的频率对打分的影响
 	 */
 	public float tf(float freq) {
+		LOG.info("freq:{}", freq);
+		return super.tf(freq);
+	}
+
+	public float tf(int freq) {
+		LOG.info("freq:{}", freq);
 		return super.tf(freq);
 	}
 
 	@Override
 	public float sloppyFreq(int distance) {
+		LOG.info("distance:{}", distance);
 		return super.sloppyFreq(distance);
 	}
 
 	@Override
 	public float scorePayload(int doc, int start, int end, BytesRef payload) {
+		LOG.info("doc:{},start:{},end:{},payload:{}", doc, start, end, payload);
 		return super.scorePayload(doc, start, end, payload);
 	}
 
 	@Override
 	public float idf(long docFreq, long numDocs) {
+		LOG.info("docFreq:{},numDocs:{}", docFreq, numDocs);
 		return super.idf(docFreq, numDocs);
 	}
 
 	@Override
 	public void setDiscountOverlaps(boolean v) {
+		LOG.info("v:{}", v);
 		super.setDiscountOverlaps(v);
 	}
 
 	@Override
 	public boolean getDiscountOverlaps() {
+		LOG.info("getDiscountOverlaps():{}", getDiscountOverlaps());
 		return super.getDiscountOverlaps();
 	}
 
